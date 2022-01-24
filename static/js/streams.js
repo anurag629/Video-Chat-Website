61d141263570acc5c849438067ccd301b32fb5dc
Your Name <you@example.com>
2022-01-24 19:08:46 +0530

    added agora sdk

---
+++ static/js/streams.js
@@ -0,0 +1,5 @@
+const app_id = 'c88a158f27d842f3872627abdee754e0'
+const CHANNEL = 'main'
+const TOCKEN = '006c88a158f27d842f3872627abdee754e0IAAUop+LaJDxx8mNMaXR6w/W+5h05CG2gcmy7EbghDh9u2TNKL8AAAAAEAD6A1gYXvrvYQEAAQBe+u9h'
+
+console.log('connected')
