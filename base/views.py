bc7f9f065061fdfc1e78df3b5a33fd322526b90f
Your Name <you@example.com>
2022-01-28 20:37:38 +0530

    added python url and view

--- base/views.py
+++ base/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random
+import time
 # Create your views here.
+
+
+def getTocken(request):
+    appId = '5d21a546d70047158c2442b150388b8d'
+    appCertificate = '89fd160f47ed425d99c2cf69ebb06453'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+    # Build token with uid
+    token = RtcTokenBuilder.buildTokenWithUid(
+        appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
 
 
 def lobby(request):
--- static/js/streams.js
+++ static/js/streams.js
@@ -1,7 +1,7 @@
 const APP_ID = 'c88a158f27d842f3872627abdee754e0'
-const CHANNEL = 'main'
-const TOCKEN = '006c88a158f27d842f3872627abdee754e0IAAUop+LaJDxx8mNMaXR6w/W+5h05CG2gcmy7EbghDh9u2TNKL8AAAAAEAD6A1gYXvrvYQEAAQBe+u9h'
-let UID;
+const CHANNEL = 'mynewchannel'
+const TOCKEN = '0065d21a546d70047158c2442b150388b8dIAA9EeTXXc8t8oSfJptNu35TmC9+Rw+MfSrBKm7r5CFrimf+nQh9K0BIIgA0o/0CAFH1YQQAAQAAUfVhAgAAUfVhAwAAUfVhBAAAUfVh'
+let UID = 215;
 
 const client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' })
 
@@ -13,7 +13,7 @@ let joinAndDisplayLocalStream = async () => {
     client.on('user-published', handleUserJoined)
     client.on('user-left', handleUserLeft)
 
-    UID = await client.join(APP_ID, CHANNEL, TOCKEN, null)
+    await client.join(APP_ID, CHANNEL, TOCKEN, UID)
 
     localTracks = await AgoraRTC.createMicrophoneAndCameraTracks()
 
@@ -58,4 +58,41 @@ let handleUserLeft = async (user) => {
     document.getElementById(`user-container-${user.uid}`).remove()
 }
 
+let leaveAndRemoveStream = async () => {
+    for (let i = 0; localTracks.length > i; i++) {
+        localTracks[i].stop()
+        localTracks[i].close()
+    }
+
+    await client.leave()
+    window.open('/', '_self')
+}
+
+let toggleCamera = async (e) => {
+    if (localTracks[1], muted) {
+        await localTracks[1].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }
+    else {
+        await localTracks[1].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(179, 0, 0)'
+    }
+}
+
+
+let toggleMic = async (e) => {
+    if (localTracks[0], muted) {
+        await localTracks[0].setMuted(false)
+        e.target.style.backgroundColor = '#fff'
+    }
+    else {
+        await localTracks[0].setMuted(true)
+        e.target.style.backgroundColor = 'rgb(179, 0, 0)'
+    }
+}
+
 joinAndDisplayLocalStream()
+
+document.getElementById('leave-btn').addEventListener('click', leaveAndRemoveStream)
+document.getElementById('camera-btn').addEventListener('click', toggleCamera)
+document.getElementById('mic-btn').addEventListener('click', toggleMic)
